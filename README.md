5f834e274e49bb14f6f984cad41ce517764b09a8
Marquis Kurt <software@marquiskurt.net>
2021-10-29 23:36:00 -0400

    :recycle: Overhaul all the things

--- README.md
+++ README.md
@@ -1,11 +1,8 @@
-# Codename "Head Over Heels"
+# Indexing Your Heart (Codename "Head Over Heels")
 
-> Your colleague takes a week-long vacation, and she tasks you with taking over her work and
-> watching over friend who's visiting. The challenges begin when her friend arrives at your doorstep
-> in a candle costume, and she asks to date you. Are you able to keep up the work on your
-> colleague's projects while fostering a romantic relationship with her friend?
-
-**Codename "Head Over Heels"** is a re-implementation of _Codename Katorin_ using native Godot tools
+**Indexing Your Heart** is an upcoming visual novel/RPG mashup where you play as a software
+engineer who has a secret admirer that you're actively dating who wears a hammer costume. The game
+is a re-implementation of _Codename Katorin_ and _Codename Coyote Hammer_, using native Godot tools
 à la C# and the Dialogic library. The primary objective of this project is to continue developing
 the game without worrying about smaller developer details that could be easily re-implemented with
 an addon.
@@ -13,7 +10,7 @@ an addon.
 > :warning: This project is still in its infancy, so some details and mechanics may not work as
 > intended.
 
-## Build from source
+## Getting started: Build from source
 
 **Required Tools**
 
@@ -32,7 +29,8 @@ dotnet CLI. Go to **Editor > Editor Settings > Mono > Builds** and change the bu
 
 ## Contributions
 
-**Codename "Head Over Heels"** includes libraries and projects under open-source licenses:
+**Indexing Your Heart (Codename "Head Over Heels")** includes libraries and projects under
+open-source licenses:
 
 - Dialogic: MIT License
 - Godot: MIT License
---
+++ utils/import_char_portraits.py
@@ -0,0 +1,67 @@
+#!/bin/python3
+
+# This script is used to re-import portraits that are located in the assets/characters folders into Dialogic, so that
+# any subsequent updates are automatically handled. This is intended to speed up production of scripts.
+
+from os import path
+from copy import deepcopy
+import os
+import json
+import sort_dialogic_imports
+
+CHARACTER_PORT_PATH = path.join("assets", "characters")
+CHARACTER_JSON_PATH = path.join("dialogic", "characters")
+
+
+def make_portait_definition(character: str, file: str) -> dict:
+    """Returns a portrait definition compatible with Dialogic
+
+    Args:
+        character (str): The name of the character the portait originates from
+        file (str): The file name of the portrait to include
+
+    Returns:
+        portrait: The portrait definition that contains the name and the path
+    """
+    return {
+        "name": file.removesuffix(".png"),
+        path: f"res://{CHARACTER_PORT_PATH}/{character}/{file}"
+    }
+
+
+def make_character_portraits(character: str) -> list:
+    """Returns a list of portraits for the specified character
+
+    Args:
+        character (str): The name of the character to fetch the portaits of
+
+    Returns:
+        portraits: The list of portrait definitions for Dialogic
+    """
+    stripped_char = character.lower().removeprefix("the ")
+    if stripped_char not in os.listdir(CHARACTER_PORT_PATH):
+        return [{"name": "Default", "path": ""}]
+    portrait_files = [
+        f for f in os.listdir(path.join(CHARACTER_PORT_PATH, stripped_char))
+        if f.endswith("png")
+    ]
+    return [make_portait_definition(stripped_char, f) for f in portrait_files]
+
+
+def replace_portraits(character_data: dict) -> dict:
+    new_character_data = deepcopy(character_data)
+    new_character_data["portaits"] = make_character_portraits(
+        new_character_data["name"])
+    return sort_dialogic_imports.sort_portraits_by_key(new_character_data)
+
+
+if __name__ == "__main__":
+    for file in os.listdir(CHARACTER_JSON_PATH):
+        if not file.endswith(".json"):
+            continue
+
+        with open(path.join(CHARACTER_JSON_PATH, file), "r") as char_file:
+            char_data = json.load(char_file)
+        new_char_data = replace_portraits(char_data)
+        with open(path.join(CHARACTER_JSON_PATH, file), "w") as char_file:
+            json.dump(new_char_data, char_file, indent=4)
